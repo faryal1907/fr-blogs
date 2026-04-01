062fd56ac9766bb1305a04cb507bbaa3139a4280
faryal1907 <faryaaal19@gmail.com>
2026-04-01 21:10:22 +0500

    guests can add articles

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask , render_template 
+from flask import Flask , render_template, request, redirect
 import json
 import os
+from datetime import datetime
+
 
 app = Flask(__name__) #an instance (my app) of the flask framework
 
@@ -33,6 +35,36 @@ def article(id):
         data = json.load(file)
 
     return render_template("article.html", article=data) 
+
+
+@app.route("/add", methods=["GET", "POST"])
+def add_article():
+    if request.method == "POST":
+        title = request.form["title"]
+        content = request.form["content"]
+        date = datetime.now().strftime("%Y-%m-%d")
+
+        # create new article
+        new_article = {
+            "title": title,
+            "content": content,
+            "date": date
+        }
+
+        # generate new ID
+        files = os.listdir("articles")
+        new_id = len(files) + 1
+
+        filepath = os.path.join("articles", f"{new_id}.json")
+
+        with open(filepath, "w") as file:
+            json.dump(new_article, file)
+
+        print(request.form)
+
+        return redirect("/")
+
+    return render_template("add.html")
 
 if __name__== "__main__":
     app.run(debug=True)
